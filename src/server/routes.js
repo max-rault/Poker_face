e3a9afc8e7dd86e06eea135945b8db6a9e4712fa
@maxRault <maxime@keematic.com>
2020-09-16 16:27:18 +0200

    work on template (pug-bootstrap)

--- src/server/routes.js
+++ src/server/routes.js
@@ -6,9 +6,19 @@ const router = express.Router()
 router.use(bodyParser.urlencoded({extended: false}))
 router.use(bodyParser.json())
 
+router.route('/')
+.get((req, res) =>{
+  // Update views
+	req.session.views = (req.session.views || 0) + 1
+	
+  // Write response
+  res.end(req.session.views + ' views')
+  res.render("mixins/index")
+})
+
 router.route('/users')
 .get((req, res) =>{
-    console.log("hello world")
+
 })
 .post((req, res) =>{
     console.log(req)
--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,24 @@
 const express = require('express')
+var cookieSession = require('cookie-session')
 const router = require('./routes')
 const app = express()
 var path = require('path');
 const port = 3000
 
 app.set('view engine', 'pug')
-app.set('views', __dirname + '/src/views')
-app.use('/static', express.static(path.join(__dirname, '/src/assets/icons')))
+app.set('views','./src/views')
+app.use('/static', express.static(path.join(__dirname, '//src///assets//icons')))
+
+app.set('trust proxy', 1) // trust first proxy
+
+app.use(cookieSession({
+  name: 'session',
+  keys: ['username', 'pwd']
+}))
 
 app.use('/', router)
 
 app.listen(port, () => {
+
   console.log(`Example app listening at http://localhost:${port}`)
 })
